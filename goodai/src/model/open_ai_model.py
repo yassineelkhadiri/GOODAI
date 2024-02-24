7be8729d5d8cfd98eaeddd0d5242a3f82b250f6e
Mohamed Yassine Elkhadiri <mohamedyassineelkhadiri@gmail.com>
2024-02-24 21:35:03 +0100

    Feat(model): add open ai model for future use

--- goodai/src/model/open_ai_model.py
+++ goodai/src/model/open_ai_model.py
@@ -14,4 +14,16 @@ class OpenAIModel(BaseModel):
         self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
     def query_llm(self, message: str) -> str:
-        return ""
+        """Query an OpenAI model and retrieve its response.
+
+        Args:
+            message: User message/question.
+
+        Returns: LLM response.
+        """
+        messages = [{"user": message}]
+        raw_response = self.client.chat.completions.create(
+            model=self.model_name, messages=messages  # type:ignore
+        )
+        formatted_response = str(raw_response.choices[0].message.content)
+        return formatted_response
