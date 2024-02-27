10cadbdac9cda7fe56b9c2dc825df00341f314e2
Mohamed Yassine Elkhadiri <mohamedyassineelkhadiri@gmail.com>
2024-02-27 20:55:50 +0100

    Feat(conversation_db): add memory retrieve methods

--- goodai/src/memory/conversation_db.py
+++ goodai/src/memory/conversation_db.py
@@ -2,11 +2,16 @@ import os
 import logging
 from typing import Dict, List
 
-from pinecone import Pinecone
+from pinecone import Pinecone, ServerlessSpec
 from dotenv import load_dotenv
 
 load_dotenv(".env")
 logger = logging.getLogger()
+
+
+_INDEX_METRIC = "cosine"
+_INDEX_DIMENSION = 768
+_SPECS = ServerlessSpec(cloud="aws", region="us-west-2")
 
 
 class ConversationDatabase:
@@ -14,13 +19,48 @@ class ConversationDatabase:
         self.api_key = os.getenv("PINECONE_API_KEY")
         self.index_name = index_name
         self.pinecone_client = Pinecone(api_key=self.api_key)
+        if index_name not in self.pinecone_client.list_indexes().names():
+            self.pinecone_client.create_index(
+                name=self.index_name,
+                dimension=_INDEX_DIMENSION,
+                metric=_INDEX_METRIC,
+                spec=_SPECS,
+            )
+
         self.pinecone_index = self.pinecone_client.Index(self.index_name)
         logger.info("Connection to database established.")
 
     def upsert_conversations(self, vectors: List[Dict]):
-        """Inser a list of vectors in the Pinecone vector database."""
+        """Insert a list of vectors in the Pinecone vector database."""
         self.pinecone_index.upsert(vectors)
 
-    # def find_similar_conversations(self, query_embedding, top_k=5):
-    #     results = pinecone.query(self.index_name, query_embedding, top_k=top_k)
-    #     return results.ids
+    def retrieve_related_memories(
+        self, encoded_memory_vector: List[float], top_k: int = 5
+    ):
+        """Retrieve examples that are most likely related to the current memory.
+
+        Args:
+            encoded_memory_vector: Encoded memory content.
+            top_k: Number of examples to fetch. Defaults to 5.
+
+        """
+        results = self.pinecone_index.query(
+            vector=encoded_memory_vector,
+            top_k=top_k,
+            include_values=False,
+            include_metadata=True,
+        )
+        return results
+
+    def clear_records(self) -> None:
+        """Delete all records in the current index."""
+        self.pinecone_client.delete_index(self.index_name)
+        self.pinecone_client.create_index(
+            name=self.index_name,
+            dimension=_INDEX_DIMENSION,
+            metric=_INDEX_METRIC,
+            spec=_SPECS,
+        )
+        self.pinecone_index = self.pinecone_client.Index(self.index_name)
+
+        logger.info("Pinecone records cleared.")
